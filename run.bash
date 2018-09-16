07776c59a7858f76ee532bf12747d2a14c3cc900
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-16 23:13:06 +0100

    use reasonml syntax

--- run.bash
+++ run.bash
@@ -1,6 +1,6 @@
+cd src && dune build index.exe
 
-
-bsc \
-  -I /home/august/.opam/4.06.1/lib/opium \
-  -bs-package-name reason-server \
-  -bs-package-output commonjs:./lib/js/ -c src/Index.re
+# bsc \
+#   -I /home/august/.opam/4.06.1/lib/opium \
+#   -bs-package-name reason-server \
+#   -bs-package-output commonjs:./lib/js/ -c src/Index.re
